9d48a4d97699c969d8a78191fe67b8baada68ba5
AzkaNaheem <41339342+AzkaNaheem@users.noreply.github.com>
2020-08-14 18:16:10 +0530

    Add files via upload

---
+++ web/Scripts/script02.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var search_input = document.querySelector("#search_input");
+
+search_input.addEventListener("keyup", function(e){
+  var span_items = document.querySelectorAll(".table_body .name span");
+  var table_body = document.querySelector(".table_body ul");
+  var search_item = e.target.value.toLowerCase();
+ 
+ span_items.forEach(function(item){
+   if(item.textContent.toLowerCase().indexOf(search_item) != -1){
+      item.closest("li").style.display = "block";
+   }
+   else{
+     item.closest("li").style.display = "none";
+     }
+ })
+  
+});
